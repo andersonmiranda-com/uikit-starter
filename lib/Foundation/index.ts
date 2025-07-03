dd2cc054528642999c347c1ade0c963cbc548db6
Anderson Miranda <me@andersonmiranda.com>
2025-07-03 14:38:06 +0200

    fix indexes

---
+++ lib/Foundation/index.ts
@@ -0,0 +1,6 @@
+export * from './Button';
+export * from './Divider';
+export * from './Heading';
+export * from './Icon';
+export * from './Link';
+export * from './Text';
--- lib/Layout/index.ts
+++ lib/Layout/index.ts
@@ -1,5 +1,6 @@
 export * from './Accordion';
 export * from './Card';
 export * from './Container';
+export * from './Grid';
 export * from './Skeleton';
 export * from './Stack';
